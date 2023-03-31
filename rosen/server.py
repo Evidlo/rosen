d407706bae672ed1c0b957ecac1c69425424d109
evan <evan@evanw.org>
2023-03-30 19:05:49 -0500

    switch server to TCP socket

--- rosen/server.py
+++ rosen/server.py
@@ -1,31 +1,27 @@
-import asyncio
-import asyncio_dgram
+import socket
 
 from rosen.gcomm import GCOMM
 
-async def udp_echo_server(host, port):
-    """Dumb echo server"""
-
-    stream = await asyncio_dgram.bind((host, port))
-
-    print(f"Serving on {stream.sockname}")
-
+async def handle_client(reader, writer):
     while True:
-        data, (remote_addr, remote_port) = await stream.recv()
+        data = await reader.readexactly(1024)
+        remote_addr = writer.get_extra_info('peername')
         g = GCOMM.parse(data)
         print(f"Received from {remote_addr}:\n    {g}")
         # reponse with ack before echoing
-        await stream.send(
-            GCOMM('ok').build(),
-            (remote_addr, remote_port)
-        )
-        await stream.send(data, (remote_addr, remote_port))
+        writer.write(GCOMM('ok').build())
+        writer.write(data)
+        await writer.drain()
         # slow the server down a bit
         await asyncio.sleep(0.3)
 
     print(f"Shutting down server")
 
+async def run_server(host, port):
+    server = await asyncio.start_server(handle_client, host, port)
+    print(f"Serving on {host} {port}")
+    async with server:
+        await server.serve_forever()
 
 def server(args):
-    loop = asyncio.get_event_loop()
-    loop.run_until_complete(asyncio.gather(udp_echo_server(args.host, args.port)))
+    asyncio.run(run_server(args.host, args.port))
