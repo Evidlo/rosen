aa6693be8b712fdf6c759780f53f9fc40e69cb89
Liam Ramsey <me@lramsey.com>
2023-08-14 16:34:52 -0500

    Updated ICOMM and AX.E. parsing to new formats. Fixed use of ICOMM n & m fields in a ICOMM script, those fields are only for use in packet splitting.

--- rosen/axe.py
+++ rosen/axe.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-from construct import Struct, ExprAdapter, Byte, GreedyBytes, Mapping, CString
+from construct import Struct, ExprAdapter, Byte, GreedyBytes, Mapping, CString, If, Int16ub, this
 from msgpack import packb, unpackb
 from rich.console import Console
 from rich.syntax import Syntax
@@ -19,29 +19,25 @@ axe = Struct(
             'statement': ord('.')
         }
     ),
-    "table" / CString('ascii'),
+    "tx_id" / If(this.cmd == 'query' or this.cmd == 'statement', Int16ub),
     # msgpack pack/unpack data
-    "data" / ExprAdapter(GreedyBytes, lambda b, _: unpackb(b), lambda b, _: packb(b))
+    "data" / ExprAdapter(GreedyBytes, lambda b, _: unpackb(b), lambda b, _: packb(b, use_single_float=True))
 )
 
 class AXE:
 
-    def __init__(self, cmd=None, data=None, table='.'):
-        self.cmd, self.data, self.table = cmd, data, table
+    def __init__(self, cmd=None, data=None, tx_id=0):
+        self.cmd, self.data, self.tx_id = cmd, data, tx_id
 
     def __repr__(self):
-        if self.table == '.':
-            s = f"{self.cmd}({self.data})"
-        else:
-            s = f"{self.cmd}({self.data}, table={self.table})"
-
+        s = f"{self.cmd}({self.data})"
         return s
 
     def build(self):
-        return axe.build({'cmd':self.cmd, 'data':self.data, 'table':self.table})
+        return axe.build({'cmd':self.cmd, 'tx_id':self.tx_id, 'data':self.data})
 
     @classmethod
     def parse(cls, raw_bytes):
         parsed = axe.parse(raw_bytes)
-        return cls(parsed.cmd, parsed.data, parsed.table)
+        return cls(parsed.cmd, parsed.data)
 
